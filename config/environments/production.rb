884bc84b70446da040bb40968130d2424ffa7cdb
dotcom900825 <dotcom900825@gmail.com>
2013-10-15 20:38:37 -0700

    precompile config missing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,10 @@ ProjectX::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.assets.precompile += %w( site_3rd_base.js site_base.js )
+  config.assets.precompile += %w( site_3rd_base.css site_base.css site_total.css )
+
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
